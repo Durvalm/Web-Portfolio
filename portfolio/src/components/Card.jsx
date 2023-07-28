e1124cf6c202236c393f63bfa21275fbcdaab913
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2023-07-28 14:19:35 -0400

    props

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -1,14 +1,17 @@
 import styles from './Card.module.css'
 
-export function Card(){
+export function Card(props){
     return (
         <div className={styles.project}>
         <div className={styles.card}>
-                <h5>Super Barbershop</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <a href="">case study</a>
-                <a href="">live demo</a>
-                <a href="">github link</a>
+                <h5>{props.title}</h5>
+                <p>{props.content}</p>
+                
+                {props.links.map(link => {
+                    return (
+                        <a href="">{link}</a>
+                    )
+                })}
         </div>
         </div>
     )
--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,12 +1,35 @@
 import styles from './Project.module.css'
 import { Card } from './Card'
 
+const cards = [
+    {
+        'id': 1,
+        'title': 'Super Barbershop',
+        'content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        'links': ['case study', 'live link', 'github']
+    },
+    {
+        'id': 2,
+        'title': 'Shippo',
+        'content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        'links': ['case study', 'github']
+    }
+]
+
 export function Project(){
     return (
     <div className={styles.projectSection}>
         <h1>Projects</h1>
-        <Card />
-        <Card />
+        {cards.map(card => {
+            return (
+                <Card 
+                    key={card.id}
+                    title={card.title}
+                    content={card.content}
+                    links={card.links}
+                />
+            )
+        })}
     </div>
     )
 }
